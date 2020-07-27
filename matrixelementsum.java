7a06e3bb418c20279c409c11d65cdc8a4d09e7f9
Luisely Doza <dluisely10999@gmail.com>
2020-07-27 07:45:02 -0400

    MatrixElementSum

---
+++ matrixelementsum.java
@@ -0,0 +1,26 @@
+class MatrixElementSum {
+
+  public static void main(String args[]) {
+    int[][] matrix = {
+      {0, 1, 1, 2},
+      {0, 5, 0, 0},
+      {2, 0, 3, 3}
+    };
+    MatrixElementSum m = new MatrixElementSum();
+    System.out.println(m.matrixElementsSum(matrix));
+  }
+
+  int matrixElementsSum(int[][] matrix) {
+    int sum = 0;
+
+    for(int i = 0; i < matrix[0].length; i++) {
+        for (int j = 0; j < matrix.length; j++) {
+            if (matrix[j][i] == 0) {
+                break;
+            }
+            sum += matrix[j][i];
+        }
+    }
+    return sum;
+  }
+}
